266d387f486fb912e3379c9ee3fc409ac91a7572
mirodil <mirsadikovmirodil52@gmail.com>
2025-09-10 14:16:33 +0500

    let's go to java

---
+++ src/main/java/uz/hello_from_go/fignya/FignyaApplication.java
@@ -0,0 +1,13 @@
+package uz.hello_from_go.fignya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FignyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FignyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/hello_from_go/fignya/FignyaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.hello_from_go.fignya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FignyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
